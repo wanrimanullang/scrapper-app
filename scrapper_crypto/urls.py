924c9486006238caf2c756804257b9f1c65ba093
rickytree <tronionio21@gmail.com>
2023-12-08 20:07:31 +0900

    Updated the scrapper

--- scrapper_crypto/urls.py
+++ scrapper_crypto/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.views.generic import RedirectView
 from .views import ScrapDataAPIReddit, ScrapDataAPIYoutube
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('search-reddit/', ScrapDataAPIReddit.as_view(), name='search_results_reddit'),
     path('reddit-topics/', ScrapDataAPIReddit.as_view(), name='reddit-topic-list'),
     path('youtube-videos/', ScrapDataAPIYoutube.as_view(), name='youtube-video-list'),
+    path('', RedirectView.as_view(url=reverse_lazy('youtube-video-list'), permanent=True), name='scrapper-dashboard')
 ]
--- scrapper_crypto/views.py
+++ scrapper_crypto/views.py
@@ -1,29 +1,25 @@
 from dotenv import load_dotenv
 import os
 
-
 from django.shortcuts import render
 from django.http import JsonResponse
 
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from praw import Reddit
 from datetime import datetime, timedelta
 from googleapiclient.discovery import build
-from datetime import datetime, timedelta
 import praw
 
 class ScrapDataAPIReddit(APIView):
     CLIENT_ID_REDDIT =  os.environ.get('CLIENT_ID_REDDIT')
     CLIENT_SECRET_REDDIT = os.environ.get('CLIENT_SECRET_REDDIT')
     SELF_AGENT = os.environ.get('USER_AGENT')
-    
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.reddit_results = []
-        
-    
+
     def get(self, request, format=None):
         reddit = praw.Reddit(client_id= self.CLIENT_ID_REDDIT,
                     client_secret=self.CLIENT_SECRET_REDDIT,
@@ -35,7 +31,7 @@ class ScrapDataAPIReddit(APIView):
         year_ago = datetime.utcnow() - timedelta(days=365)
 
         reddit_results = []
-        
+
         for submission in subreddit.search(search_keywords, time_filter='year'):
             if submission.created_utc >= year_ago.timestamp():
                 reddit_results.append({
@@ -43,33 +39,35 @@ class ScrapDataAPIReddit(APIView):
                     'URL': submission.url,
                     'Posted': datetime.utcfromtimestamp(submission.created_utc).isoformat()
                 })
+        # return Response({
+        #     'reddit_results': reddit_results,
+        # })
         return render(request, 'reddit_results.html', {'reddit_results': reddit_results})
-        
+
 class ScrapDataAPIYoutube(APIView):
     API_KEY_YOUTUBE = os.environ.get('API_KEY_YOUTUBE')
-    
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.youtube_results = []
-    
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
-    
+
     def get(self, request, format=None, *args, **kwargs):
         api_key = self.API_KEY_YOUTUBE
         youtube = build('youtube', 'v3', developerKey=api_key)
 
         search_keywords = request.GET.get('search_keywords', '')
         year_ago = datetime.utcnow() - timedelta(days=365)
-        
-        youtube_results = []
+
         search_response = []
-        
+
         search_response = youtube.search().list(
             q=search_keywords,
             part='id,snippet',
             type='video',
             maxResults=20
             ).execute()
-        
+
         return render(request, 'youtube_results.html', {'youtube_results': search_response})
